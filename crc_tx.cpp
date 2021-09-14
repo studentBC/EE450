b141840783acb7d0abaa86b69af4c907afbc8d24
studentBC <r04546032@ntu.edu.tw>
2021-09-14 13:03:13 -0700

    there is a bug in division, lets find it

--- crc_tx.cpp
+++ crc_tx.cpp
@@ -1,63 +1,59 @@
 #include "crc.h"
+#include <algorithm>
 #define crc12 "1100000001111"
 #define rcrc12 "1111000000011"
 using namespace std;
-bitset<13> compute(bitset<1000>temp) {
+bitset<13> compute(bitset<1000>temp, int length) {
 	bitset<13>c12(rcrc12), checksum;
-	cout <<"----------------------------------" << endl;
-	for (int i= 0; i < 13; i++) cout << c12[i];
-	cout <<"----------------------------------" << endl;
+	//cout << "original c12: " <<c12.to_string() << endl;
 	//cout <<"original data: " << temp.to_string() << endl;
-	temp<<=12;
 	int pos = 0;
-	for (int i = 0; i < 1000; i++) {
-		if (temp[i] == 1) {
-			pos = i;
-			for (int j = i, k = 0; k < 13 && j < 1000; j++,k++) {
-				checksum[k] = temp[j];
-			}
-			break;
-		}
+	while (!temp[pos]) {
+		temp>>=1;
 	}
+	for (int i= 0; i < 13; i++) checksum[i] = temp[i];
 	bitset<1000>ans;
-	cout <<"begin at pos: " << pos << endl;
-	pos+=13;
-	while (pos < 987) {
-		cout <<"----------------------------------" << endl;
+	//cout <<"begin at pos: " << pos << endl;
+	pos=13;
+	while (pos < length) {
+		
+		cout <<"==================================" << endl;
 		for (int i= 0; i < 13; i++) cout << checksum[i];
 		cout << endl << "pos: " << pos << endl;
-		cout <<"----------------------------------" << endl;
+		cout <<"==================================" << endl;
+			 
 		//cout <<"we got:  " << checksum.to_string() << endl;
-		while (checksum[0] == 0) {
-			ans[999] = 0;
+		while (!checksum[0]) {
+			ans[0] = 0;
 			ans<<=1;
-			checksum<<=1;
+			checksum>>=1;
 			checksum[12] = temp[pos];
 			pos++;
-			cout <<"pos -> " << pos << endl;
 		}
 		checksum^=c12;
-		ans[999] = 1;
+		ans[0] = 1;
 		ans<<=1;
 	}
 	cout <<"----------------answer-----------------" << endl << endl;
-	for (int i= 0; i < 13; i++) cout << checksum[i];
+	//for (int i= 0; i < 13; i++) cout << checksum[i];
+	cout <<"crc  : " << checksum.to_string() << endl;
 	cout <<"----------------------------------" << endl;
 	//cout <<"we got divide: " << ans.to_string() << endl;
+	//010111100100
 	return checksum; 
 }
 int main () {
 	string line;
 	vector<bitset<1000> >data;
+	vector<int>len;
 	ifstream infile("dataTx.txt");
 	//start to read file;
 	while (getline(infile, line)) {
-		bitset<1000>tmp(line);
-		/*for (int i = line.size()-1, j = 999; i > -1; i--, j--) {
-			if (line[i] == '1') tmp[j] = 1;
-			else tmp[j] = 0;
-		}*/
 		cout << line << endl;
+		reverse(line.begin(), line.end());
+		bitset<1000>tmp(line);
+		len.push_back(line.size());
+		//cout << tmp.to_string() << endl;
 		data.push_back(tmp);
 	}
 	//lets compute crc checksum
@@ -65,7 +61,7 @@ int main () {
 	for (int i = 0; i < data.size(); i++) {
 		//cout <<"codeword:" << data[i].to_string() << endl;
 		//cout <<"crc:" << compute(data[i]).to_string()  << endl;
-		compute(data[i]);
+		compute(data[i], len[i]+12);
 	}
 	return 0;
 }
